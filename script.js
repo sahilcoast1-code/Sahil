9b227eec7384ea1e0550fab52bbd2e53a783fc01
sahilcoast1-code <sahilcoast1@gmail.com>
2026-07-12 16:36:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// Navbar Effect
-
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-
-if(window.scrollY > 50){
-
-header.style.background = "rgba(0,0,0,.75)";
-header.style.padding = "15px 7%";
-
-}else{
-
-header.style.background = "rgba(0,0,0,.35)";
-header.style.padding = "20px 7%";
-
-}
-
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
