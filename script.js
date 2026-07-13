64c4c24c7ec94e60482bb36c8a8437a8ff49eb42
sahilcoast1-code <sahilcoast1@gmail.com>
2026-07-13 16:35:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
+// SAHIL COAST LANGUAGE SYSTEM
 
+
+function changeLanguage(lang){
+
+
+    // حفظ اللغة
+
+    localStorage.setItem("language", lang);
+
+
+
+    // تغيير اتجاه الموقع
+
+    if(lang === "ar" || lang === "he"){
+
+        document.documentElement.dir = "rtl";
+
+    } else {
+
+        document.documentElement.dir = "ltr";
+
+    }
+
+
+
+    // تغيير النصوص
+
+    document.querySelectorAll("[data-en]").forEach(element => {
+
+
+        element.innerHTML = element.dataset[lang];
+
+
+    });
+
+
+
+    // إخفاء شاشة اللغة
+
+    let screen = document.getElementById("languageScreen");
+
+
+    if(screen){
+
+        screen.style.opacity = "0";
+
+
+        setTimeout(()=>{
+
+            screen.style.display = "none";
+
+        },500);
+
+    }
+
+
+}
+
+
+
+
+// عند فتح الموقع
+
+
+window.addEventListener("load", ()=>{
+
+
+    let savedLanguage = localStorage.getItem("language");
+
+
+
+    if(savedLanguage){
+
+
+        changeLanguage(savedLanguage);
+
+
+    }
+
+
+});
